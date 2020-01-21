d07ddd47e1e8709a2c270aff631c8b0f49da097a
GsWahyu <wahyupurnama434@gmail.com>
2020-01-22 06:39:54 +0800

    menambahkan file php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+	<h1>Belajar remote</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae obcaecati, quaerat vero, placeat quo repellendus dolores nobis velit veritatis non asperiores. Blanditiis animi reiciendis molestias sed dolore quis aperiam voluptas.</p>
+</body>
+</html>
