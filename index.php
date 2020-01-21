f7bf6a366a95e7c9adb50702745df6900b37e29a
GsWahyu <wahyupurnama434@gmail.com>
2020-01-22 06:44:28 +0800

    menambahkan pargraf ke 3

--- index.php
+++ index.php
@@ -9,5 +9,6 @@
 	<h1>Belajar remote</h1>
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae obcaecati, quaerat vero, placeat quo repellendus dolores nobis velit veritatis non asperiores. Blanditiis animi reiciendis molestias sed dolore quis aperiam voluptas.</p>
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, qui, eius? Itaque vel, labore quia illum nesciunt, eos voluptatum, totam pariatur similique numquam adipisci ratione minus a consectetur eligendi aut.</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum quae soluta, ipsum necessitatibus, saepe, quibusdam quos provident repellat nam quis, unde temporibus. Incidunt laboriosam blanditiis doloribus in quis, ullam distinctio.</p>
 </body>
 </html>
